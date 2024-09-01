6027798682855fc5bbd12760e1c260e90dfa0561
nandanbasak <nandanbasak@gmail.com>
2024-09-01 21:44:24 +0530

    changed loging page style

--- DemoApp/src/app/login/login.component.ts
+++ DemoApp/src/app/login/login.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  visible:boolean=false;
+  changetype:boolean=true;
   constructor() { }
 
   ngOnInit() {
   }
-
+viewpass(){
+  this.visible=!this.visible;
+  this.changetype=!this.changetype;
+}
 }
